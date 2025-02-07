c2508505ea8dc291cf98ce276d8fc8f7659096d6
AdMub <admub465@gmial.com>
2025-02-07 09:50:12 +0000

    Added Pluto notebook and project files

---
+++ variables.jl
@@ -0,0 +1,15 @@
+#=
+# Assign values to variables
+x = 42         # Integer
+pi = 3.14      # Floating-point number
+name = "Julia" # String
+
+name = "Alice"
+age = 30
+# Simple interpolation
+message = "Hi, I’m $name and I’m $age years old."
+println(message)  # Output: Hi, I’m Alice and I’m 30 years old.
+# Interpolating expressions
+calculation = "5 + 10 equals $(5 + 10)."
+println(calculation)  # Output: 5 + 10 equals 15.
+=#
